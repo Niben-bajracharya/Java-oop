3fb8bc24f1e13522b304acc9047f69eed2144fc4
Niben-bajracharya <163286760+Niben-bajracharya@users.noreply.github.com>
2024-03-13 15:26:20 +0545

    Add files via upload

---
+++ TestFinally.java
@@ -0,0 +1,20 @@
+public class TestFinally{
+    public static void main(String args[]){
+        try{
+            System.out.println("Inside the try block");
+            //below code throws divide by zero exception
+            int data=25/0;
+            System.out.println(data);
+        }
+        //cannot handle Arithmetic type exception
+        //can only accept Null pointer type exception
+        catch(NullPointerException e){
+            System.out.println(e);
+        }
+        //executes regardles of exception occured or not
+        finally{
+            System.out.println("finally block is always executed");
+        }
+        System.out.println("rest of the code....");
+    }
+}
